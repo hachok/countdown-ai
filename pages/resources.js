db9654e8afd71e61a704c7d2c3a85a8f1d6c69c2
Dmytro Hachok <dmytro.hachok@maxwell.ai>
2019-08-20 23:57:31 +0200

    fetch data

---
+++ pages/resources.js
@@ -0,0 +1,55 @@
+import { Card, Layout, Page } from "@shopify/polaris";
+import gql from 'graphql-tag';
+import { Query } from 'react-apollo';
+import { Card } from '@shopify/polaris';
+
+const GET_PRODUCTS_BY_ID = gql`
+  query getProducts($ids: [ID!]!) {
+    nodes(ids: $ids) {
+      ... on Product {
+        title
+        handle
+        descriptionHtml
+        id
+        images(first: 1) {
+          edges {
+            node {
+              originalSrc
+              altText
+            }
+          }
+        }
+        variants(first: 1) {
+          edges {
+            node {
+              price
+              id
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
+const Resources = () => (
+  <Page>
+    <Layout>
+      <Card>
+        <Query query={GET_PRODUCTS_BY_ID}>
+        {({ data, loading, error }) => {
+          if (loading) return <div>Loading…</div>;
+          if (error) return <div>{error.message}</div>;
+          console.log(data);
+          return (
+            <Card>
+              <p>stuff here</p>
+            </Card>
+          );
+        }}
+        </Query>
+      </Card>
+    </Layout>
+  </Page>
+);
+export default Resources;
