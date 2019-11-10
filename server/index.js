963180b3f99717ecfab58a4fc2ab85e87432a6d0
Dmytro Hachok <dmytro.hachok@maxwell.ai>
2019-11-10 14:21:31 +0100

    add graphql-import

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 require('@babel/register')({
   presets: ['@babel/preset-env'],
   ignore: ['node_modules'],
-  plugins: ['@babel/plugin-transform-spread'],
+  plugins: ['@babel/plugin-transform-spread', 'import-graphql'],
 });
 
 // Import the rest of our application.
