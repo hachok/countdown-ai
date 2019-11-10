e6c60bf691eabcbeeff28971e8808e45a596bc2f
Dmytro Hachok <dmytro.hachok@maxwell.ai>
2019-11-10 03:52:57 +0100

    update paths

--- server/server.js
+++ server/server.js
@@ -21,9 +21,9 @@ const app = next({
 });
 const handle = app.getRequestHandler();
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY, SCOPES } = process.env;
-const typeDefs = importSchema('./server/gql/schema.graphql');
+const typeDefs = importSchema('/server/gql/schema.graphql');
 const db = new Prisma({
-  typeDefs: './prisma/generated/prisma.graphql',
+  typeDefs: '/prisma/generated/prisma.graphql',
   endpoint: "https://countdown-43264fa942.herokuapp.com/countdown-service/countdown-stage",
   debug: true,
 });
