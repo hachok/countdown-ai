6ce54412bbb5bcb5ad655b7fa6334be73ece1b0a
Dmytro Hachok <dmytro.hachok@maxwell.ai>
2019-11-12 23:41:57 +0100

    change server aproach

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ import "@babel/polyfill";
 import dotenv from "dotenv";
 import "isomorphic-fetch";
 import createShopifyAuth, { verifyRequest } from "@shopify/koa-shopify-auth";
-import graphQLProxy, { ApiVersion } from "@shopify/koa-shopify-graphql-proxy";
 import Koa from "koa";
 import next from "next";
 import Router from "koa-router";
@@ -11,7 +10,16 @@ import { ApolloServer } from "apollo-server-koa";
 import { Mutation } from "./gql/Mutation";
 import { Query } from "./gql/Query";
 import { Prisma } from "prisma-binding";
-import {importSchema} from 'graphql-import';
+import { importSchema } from "graphql-import";
+import { HttpLink } from "apollo-link-http";
+import fetch from "node-fetch";
+import {
+  introspectSchema,
+  makeRemoteExecutableSchema,
+  makeExecutableSchema,
+  mergeSchemas
+} from "graphql-tools";
+import { setContext } from "apollo-link-context";
 
 dotenv.config();
 const port = parseInt(process.env.PORT, 10) || 8081;
@@ -21,37 +29,22 @@ const app = next({
 });
 const handle = app.getRequestHandler();
 const { SHOPIFY_API_SECRET_KEY, SHOPIFY_API_KEY, SCOPES } = process.env;
-const typeDefs = importSchema('server/schema.graphql');
+const typeDefs = importSchema("server/schema.graphql");
 const db = new Prisma({
-  typeDefs: 'prisma/generated/prisma.graphql',
-  endpoint: "https://eu1.prisma.sh/dmytro-hachok-b9054e/countdown-service/countdown-stage",
-  debug: true,
+  typeDefs: "prisma/generated/prisma.graphql",
+  endpoint:
+    "https://eu1.prisma.sh/dmytro-hachok-b9054e/countdown-service/countdown-stage",
+  debug: true
 });
 
-const graphQLServer = new ApolloServer({
-  typeDefs,
-  resolvers: {
-    Mutation,
-    Query
-  },
-  // Make graphql playgroud available at /graphql
-  playground: {
-    endpoint: "/graphql"
-  },
-  bodyParser: true,
-  context: ({ req }) => ({
-    ...req,
-    db
-  })
-});
-
-app.prepare().then(() => {
+app.prepare().then(async () => {
   const server = new Koa();
   const router = new Router();
+  let token = '';
   server.use(session(server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
 
-  server.use(
+  await server.use(
     createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
@@ -59,24 +52,62 @@ app.prepare().then(() => {
       async afterAuth(ctx) {
         //Auth token and shop available in session
         //Redirect to shop upon auth
-        const { shop } = ctx.session;
+        const { shop, accessToken } = ctx.session;
+        token = accessToken;
         ctx.cookies.set("shopOrigin", shop, { httpOnly: false });
         ctx.redirect("/");
       }
     })
   );
 
+  const http = new HttpLink({
+    uri: `/admin/api/2019-07/graphql.json`,
+    fetch
+  });
+
+  const gqlSchema = makeExecutableSchema({
+    typeDefs,
+    resolvers: {
+      Mutation,
+      Query
+    }
+  });
+
+  const link = setContext((request, previousContext) => ({
+    headers: {
+      "Content-Type": "application/json",
+      "X-Shopify-Access-Token": token
+    }
+  })).concat(http);
+
+  const schema = await introspectSchema(http);
+
+  const shopifySchema = makeRemoteExecutableSchema({ schema, link });
+
+  const mergedSchema = mergeSchemas({
+    schemas: [gqlSchema, shopifySchema]
+  });
+
+  const graphQLServer = new ApolloServer({
+    schema: mergedSchema,
+    context: ({ req }) => ({
+      ...req,
+      db
+    })
+  });
+
   graphQLServer.applyMiddleware({
     app: server
   });
-
-  server.use(graphQLProxy({ version: ApiVersion.July19 }));
 
   router.get("*", verifyRequest(), async ctx => {
     await handle(ctx.req, ctx.res);
     ctx.respond = false;
     ctx.res.statusCode = 200;
   });
+
+  server.use(router.allowedMethods());
+  server.use(router.routes());
 
   server.listen(port, () => {
     console.log(`> Ready on http://localhost:${port}`);
