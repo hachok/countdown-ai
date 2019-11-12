3c3b837be4b42539c86193d0f4afe1ce31f9b26a
Dmytro Hachok <dmytro.hachok@maxwell.ai>
2019-11-12 19:19:07 +0100

    change query

--- components/App/App.js
+++ components/App/App.js
@@ -9,11 +9,13 @@ import { Query, withApollo } from "react-apollo";
 const img = "https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg";
 
 const GET_USERS = gql`
-  {
+  query {
     shop {
-      id
       name
-      email
+      primaryDomain {
+        url
+        host
+      }
     }
   }
 `;
--- pages/resources.js
+++ pages/resources.js
@@ -35,18 +35,6 @@ const Resources = () => (
   <Page>
     <Layout>
       <Card>
-        <Query query={GET_USERS}>
-          {({ data, loading, error }) => {
-            if (loading) return <div>Loading…</div>;
-            if (error) return <div>{error.message}</div>;
-            console.log(data);
-            return (
-              <Card>
-                <p>stuff here</p>
-              </Card>
-            );
-          }}
-        </Query>
         <Query query={GET_PRODUCTS_BY_ID}>
         {({ data, loading, error }) => {
           if (loading) return <div>Loading…</div>;
