9f0894ccad9af49f0496a8b25d28af82ef52e90d
Dmytro Hachok <dmytro.hachok@maxwell.ai>
2019-11-30 23:31:36 +0100

    revert

--- server/server.js
+++ server/server.js
@@ -38,21 +38,14 @@ const db = new Prisma({
   debug: true
 });
 
-app.prepare().then(async () => {
+app.prepare().then(() => {
   const server = new Koa();
   const router = new Router();
-  let _settings = {
-    token: "",
-    shop: ""
-  };
   server.use(session(server));
   server.keys = [SHOPIFY_API_SECRET_KEY];
 
-  console.log("starts here");
-
-  server.use(() => {
-    console.log("before auth");
-    return createShopifyAuth({
+  server.use(
+    createShopifyAuth({
       apiKey: SHOPIFY_API_KEY,
       secret: SHOPIFY_API_SECRET_KEY,
       scopes: [SCOPES],
@@ -60,70 +53,14 @@ app.prepare().then(async () => {
         //Auth token and shop available in session
         //Redirect to shop upon auth
         const { shop, accessToken } = ctx.session;
-        _settings.token = accessToken;
-        _settings.shop = shop;
-        await console.log("before accessToken ----------------- ", accessToken);
-        await console.log("before shop ----------------- ", shop);
         await db.mutation.createUser({ data: { name: "1", surname: "1" } });
         ctx.cookies.set("shopOrigin", shop, { httpOnly: false });
         ctx.redirect("/");
       }
-    });
-  });
+    })
+  );
+
   server.use(graphQLProxy({ version: ApiVersion.July19 }));
-
-  console.log("after here");
-
-  // server.use(() => {
-  //   console.log("graphqlMiddleware");
-  //   return async function graphqlMiddleware(ctx, next) {
-  //     console.log("ctx ??????????", ctx.session);
-  //     console.log("accessToken ????????", ctx.session.accessToken);
-  //     try {
-  //       const gqlSchema = makeExecutableSchema({
-  //         typeDefs,
-  //         resolvers: {
-  //           Mutation,
-  //           Query
-  //         }
-  //       });
-  //
-  //       const http = new HttpLink({
-  //         uri: `https://demo-sample-store1.myshopify.com/admin/api/graphql.json`,
-  //         fetch
-  //       });
-  //
-  //       const link = setContext(() => ({
-  //         headers: {
-  //           "Content-Type": "application/json",
-  //           "X-Shopify-Access-Token": ctx.session.accessToken
-  //         }
-  //       })).concat(http);
-  //
-  //       const schema = await introspectSchema(http);
-  //
-  //       const shopifySchema = makeRemoteExecutableSchema({ schema, link });
-  //
-  //       const mergedSchema = mergeSchemas({
-  //         schemas: [gqlSchema, shopifySchema]
-  //       });
-  //
-  //       const graphQLServer = new ApolloServer({
-  //         schema: mergedSchema,
-  //         context: ({ req }) => ({
-  //           ...req,
-  //           db
-  //         })
-  //       });
-  //       graphQLServer.applyMiddleware({
-  //         app: server
-  //       });
-  //     } catch (e) {
-  //       console.log("e", e);
-  //     }
-  //     await next();
-  //   };
-  // });
 
   router.get("*", verifyRequest(), async ctx => {
     await handle(ctx.req, ctx.res);
