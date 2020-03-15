77613c52e6d025c049c7c4384ff02285da21907b
Dmytro Hachok <d.gachok@gmail.com>
2020-03-15 17:38:31 +0100

    put try catch

--- server/shopifySchema.js
+++ server/shopifySchema.js
@@ -1,22 +1,28 @@
 import fetch from "node-fetch";
 import { introspectSchema, makeRemoteExecutableSchema } from "graphql-tools";
 import { GRAPHQL_PATH_PREFIX } from "./proxy";
+import { importSchema } from "graphql-import";
+const typeDefs = importSchema("server/schema.graphql");
 
 const shopifySchema = async ({ query, variables, operationName, ctx }) => {
   console.log("ctx", ctx);
   const { accessToken } = ctx.session;
-
-  const getSchema = await fetch(
-    `https://${GRAPHQL_PATH_PREFIX}/2019-04/graphql.json`,
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        "X-Shopify-Access-Token": accessToken
+  try {
+    const getSchema = await fetch(
+      `https://${GRAPHQL_PATH_PREFIX}/2019-04/graphql.json`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          "X-Shopify-Access-Token": accessToken
+        }
       }
-    }
-  );
-  return getSchema.json();
+    );
+    return getSchema.json();
+  } catch (e) {
+    console.log("eeeroro", e);
+  }
+  return { typeDefs };
 };
 
 export default async () => {
